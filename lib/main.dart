57108b34241791aab83756d0b5f90feb905d2900
Abdullah-Uppal <computerinsider4@gmail.com>
2023-08-03 17:11:21 +0500

    web frame

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:task_1/app_state.dart';
@@ -20,36 +22,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        textTheme: GoogleFonts.openSansTextTheme(),
-        // fontFamily: GoogleFonts.openSans().fontFamily,
-        useMaterial3: true,
-      ),
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case "/programs":
-            return MaterialPageRoute(
-              builder: (context) => const ViewAllProgramsPage(),
-            );
-          case "/lessons":
-            return MaterialPageRoute(
-              builder: (context) => const ViewAllLessonsPage(),
-            );
-          default:
-            return MaterialPageRoute(
-              builder: (context) => const Scaffold(
-                body: Center(
-                  child: Text("404"),
-                ),
-              ),
-            );
-        }
-      },
-      home: const HomePage(),
+    return FlutterWebFrame(
+      enabled: kIsWeb,
+      backgroundColor: Colors.white,
+      maximumSize: const Size(475, double.infinity),
+      builder: (context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            textTheme: GoogleFonts.openSansTextTheme(),
+            // fontFamily: GoogleFonts.openSans().fontFamily,
+            useMaterial3: true,
+          ),
+          onGenerateRoute: (settings) {
+            switch (settings.name) {
+              case "/programs":
+                return MaterialPageRoute(
+                  builder: (context) => const ViewAllProgramsPage(),
+                );
+              case "/lessons":
+                return MaterialPageRoute(
+                  builder: (context) => const ViewAllLessonsPage(),
+                );
+              default:
+                return MaterialPageRoute(
+                  builder: (context) => const Scaffold(
+                    body: Center(
+                      child: Text("404"),
+                    ),
+                  ),
+                );
+            }
+          },
+          home: const HomePage(),
+        );
+      }
     );
   }
 }
